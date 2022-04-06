b45eb16cc324eac46f082d6d23fd2667c925caef
mahdy.jannata <mahdy.jannata@delameta.com>
2022-04-06 14:22:48 +0700

    Testing buka port

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from curses import baudrate
+import serial
+import sqlite3
+
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=4800,parity=serial.PARITY_NONE)
+print("connected to: " + ser.portstr)
+print(ser.readline())
